2be5a73d2d699336c36da355457ba5569fbcee44
Tyler Haglund <relyt4me@gmail.com>
2020-08-19 15:43:15 -0600

    Add the background images to each movie-card

--- src/Movie-card.js
+++ src/Movie-card.js
@@ -1,9 +1,9 @@
 import React from 'react';
-// import './Movie-Section.css';
+import './Movie-Card.css';
 
 const MovieCard = ({ movie }) => {
   return (
-    <article className='movie-card'>
+    <article className='movie-card' style={{ backgroundImage: `url(${movie.poster_path})` }}>
       <h3 className='movie-card-title'>{movie.title}</h3>
       <p className='movie-card-rating'>{movie.average_rating}</p>
     </article>
