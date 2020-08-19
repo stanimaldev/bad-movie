397998b60963a4eb556c8c5b13e321ea183f886b
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-08-19 15:25:42 -0600

    Add img element for logo #19

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,7 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header">
+      <img src="" alt="Bad Movie Logo" height="50" width="50" />
       <h1 className="title">Bad Movie</h1>
       <button className="login-button">Login</button>
     </header>
