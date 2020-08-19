3bad7820ae838eee74f0af5585e9d0243c5a4090
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-08-19 15:03:01 -0600

    Create Header functional component with title and login button #17

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="title">Bad Movie</h1>
+      <button className="login-button">Login</button>
+    </header>
+  )
+}
+
+export default Header;
