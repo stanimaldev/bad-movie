dbc2f01326969707cbbd9896ec7b42498689d450
Tyler Haglund <relyt4me@gmail.com>
2020-08-19 15:51:47 -0600

    Remove Title of movie

--- src/Movie-card.js
+++ src/Movie-card.js
@@ -4,7 +4,6 @@ import './Movie-Card.css';
 const MovieCard = ({ movie }) => {
   return (
     <article className='movie-card' style={{ backgroundImage: `url(${movie.poster_path})` }}>
-      <h3 className='movie-card-title'>{movie.title}</h3>
       <p className='movie-card-rating'>{movie.average_rating}</p>
     </article>
   );
