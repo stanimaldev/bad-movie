7ec740e958fbcf6b7f13400bf0638babbf283071
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-08-20 15:15:58 -0600

    Add necessary imports

---
+++ src/Movie-Card.test.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import Movie-Card from './Movie-Section';
+import { screen, fireEvent, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
---
+++ src/Movie-Section.test.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import Movie-Section from './Movie-Section';
+import { screen, fireEvent, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
