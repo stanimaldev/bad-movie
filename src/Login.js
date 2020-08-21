6b72bcc8b2b7f1fd6c990ebc207bd26c0c8e752f
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-08-20 20:44:13 -0600

    Make Login a class component

--- src/Login.js
+++ src/Login.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Login.css';
 
-const Login = ({ toggleLoginModal }) => {
+class Login extends Component {
+  // = ({ toggleLoginModal }) =>
+  constructor() {
+    super()
+    this.state={
+      username: '',
+      password: ''
+    }
+  }
+
+  attemptLogin = (event) => {
+    event.preventDefault();
+  }
+
   return (
     <div className='bg-login-modal' onClick={toggleLoginModal}>
-      <form className='login-form' onClick={(e) => e.stopPropagation()}>
+      <form className='login-form' onClick={(e) => e.stopPropagation()} onSubmit={attemptLogin}>
         <h3 className='login-header'>Login</h3>
         <label for='username' class='username'>
           username:
         </label>
-        <input id='username' maxlength='10' class='username-input' type='text' />
+        <input id='username' maxlength='10' class='username-input' type='text' onChange={updateForm} />
         <label class='username-alert hide'>username is required</label>
         <label for='password' class='password'>
           password:
         </label>
-        <input id='password' maxlength='12' class='password-input' type='password' />
+        <input id='password' maxlength='12' class='password-input' type='password' onChange={updateForm} />
         <label class='password-alert hide'>password is required</label>
         <label class='invalid-alert hide'>invalid username, please try again</label>
         <button class='submit' aria-label='Submit'>
