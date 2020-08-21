e54d053c01e036ca0a2af367576134782eed81ed
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-08-20 20:01:05 -0600

    Add setup for testing

--- src/Movie-Section.test.js
+++ src/Movie-Section.test.js
@@ -1,4 +1,16 @@
 import React from 'react';
-import Movie-Section from './Movie-Section';
+import MovieSection from './Movie-Section';
 import { screen, fireEvent, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
+
+describe('Movie-Section Component', () => {
+  it('Should have the correct content when rendered', () => {
+    // render (<Movie-Section
+    //   movies={["Greenland", "Archive", "Akira"]}
+    //   />)
+    //
+    //   const movies = screen.getByText(["Greenland", "Archive", "Akira"])
+    //
+    //   expect(movies).toBeInTheDocument();
+  });
+})
