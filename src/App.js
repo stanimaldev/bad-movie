29971dcaf26ea0b4760d59fcfb77eb7eb8ff8c9a
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-08-22 12:33:47 -0600

    Add showMoviePage and displayMoviePage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './Header';
 import Login from './Login';
 import MovieSection from './Movie-Section';
 import Fetch from './Fetch';
+import MoviePage from './Movie-Page';
 
 class App extends Component {
   constructor() {
@@ -12,7 +13,8 @@ class App extends Component {
       movies: [],
       error: '',
       showLoginModal: false,
-      currentUser: false
+      currentUser: false,
+      showMoviePage: false,
     };
     this.fetch = new Fetch();
   }
@@ -30,6 +32,11 @@ class App extends Component {
     this.setState({ currentUser: false })
   }
 
+  displayMoviePage = () => {
+    console.log("movie clicked")
+    this.setState({ showMoviePage: !this.state.showMoviePage });
+  }
+
   componentDidMount() {
     this.fetch
       .getAllMovies()
@@ -42,13 +49,14 @@ class App extends Component {
   }
 
   render() {
-    const { movies, error, showLoginModal } = this.state;
+    const { movies, error, showLoginModal, showMoviePage } = this.state;
     return (
       <div className='App'>
         <Header toggleLoginModal={this.toggleLoginModal} logoutUser={this.logoutUser} currentUser={this.state.currentUser} />
         {error && <h2>{error}</h2>}
         <MovieSection movies={movies} />
-        {showLoginModal && <Login toggleLoginModal={this.toggleLoginModal} changeUser={this.changeUser} />}
+        {showLoginModal && <Login toggleLoginModal={this.toggleLoginModal} changeUser={this.changeUser} displayMoviePage={this.displayMoviePage}/>}
+        {showMoviePage && <MoviePage />}
       </div>
     );
   }
