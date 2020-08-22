0eadefc0b99539af738966eeb828c31879f451f4
Tyler Haglund <relyt4me@gmail.com>
2020-08-22 15:32:52 -0600

    Add descriptitons to the Movie Section

--- src/Movie-Section.js
+++ src/Movie-Section.js
@@ -4,21 +4,22 @@ import './Movie-Section.css';
 
 class MovieSection extends Component {
   constructor(props) {
-    super(props)
-    this.state = {
-    };
-  };
+    super(props);
+    this.state = {};
+  }
 
   render() {
-    const movieCards = this.props.movies.map((movie) => { return <MovieCard movie={movie} key={movie.id} displayMoviePage={this.props.displayMoviePage} /> })
+    const movieCards = this.props.movies.map((movie) => {
+      return <MovieCard movie={movie} key={movie.id} displayMoviePage={this.props.displayMoviePage} />;
+    });
 
     return (
       <section className='movies-section'>
-      <h2 className='movies-section-header'>Movies</h2>
-      <div className='movies-section-container'>{movieCards}</div>
+        <h2 className='movies-section-header'>Movies</h2>
+        <div className='movies-section-container'>{movieCards}</div>
       </section>
     );
-  };
-};
+  }
+}
 
 export default MovieSection;
