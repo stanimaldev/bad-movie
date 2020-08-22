727c8ec74e34b412fd3f67c78f4eaf7b5a83b5e2
Tyler Haglund <relyt4me@gmail.com>
2020-08-22 12:51:34 -0600

    Create test for toggleLogin fire

--- src/Header.test.js
+++ src/Header.test.js
@@ -3,7 +3,7 @@ import Header from './Header';
 import { screen, fireEvent, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
-describe('Header Component', () =>
+describe('Header Component', () => {
   it('Should have the correct content when rendered without a current User', () => {
     render(<Header currentUser={false} toggleLoginModal={jest.fn()} logoutUser={jest.fn()} />);
 
@@ -14,4 +14,15 @@ describe('Header Component', () =>
     expect(badMovieTitle).toBeInTheDocument();
     expect(badMovieLogo).toBeInTheDocument();
     expect(loginButton).toBeInTheDocument();
-  }));
+  });
+
+  it('Should fire a function when the login button is clicked', () => {
+    const mockToggleLogin = jest.fn();
+    render(<Header currentUser={false} toggleLoginModal={mockToggleLogin} logoutUser={jest.fn()} />);
+
+    const button = screen.getByText('Login');
+    fireEvent.click(button);
+
+    expect(mockToggleLogin).toBeCalledTimes(1);
+  });
+});
