0a92aa55e78930f026e7c9cf3d137e70a846d79c
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-08-22 13:24:46 -0600

    Pass displayMoviePage method through to Movie Card

--- src/Movie-Section.js
+++ src/Movie-Section.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MovieCard from './Movie-Card';
 import './Movie-Section.css';
 
-const MovieSection = ({ movies }) => {
-  const movieCards = movies.map((movie) => {
-    return <MovieCard movie={movie} key={movie.id} />;
-  });
+class MovieSection extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    };
+  };
 
-  return (
-    <section className='movies-section'>
+  render() {
+    const movieCards = this.props.movies.map((movie) => { return <MovieCard movie={movie} key={movie.id} displayMoviePage={this.props.displayMoviePage} /> })
+
+    return (
+      <section className='movies-section'>
       <h2 className='movies-section-header'>Movies</h2>
       <div className='movies-section-container'>{movieCards}</div>
-    </section>
-  );
+      </section>
+    );
+  };
 };
 
 export default MovieSection;
