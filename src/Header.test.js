0610fceaf12886e1cc464e03724535c9aa7bf9f2
Tyler Haglund <relyt4me@gmail.com>
2020-08-22 12:39:55 -0600

    Create test for render without current User

--- src/Header.test.js
+++ src/Header.test.js
@@ -1,4 +1,17 @@
 import React from 'react';
-import MovieCard from './Movie-Card';
+import Header from './Header';
 import { screen, fireEvent, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
+
+describe('Header Component', () =>
+  it('Should have the correct content when rendered without a current User', () => {
+    render(<Header currentUser={false} toggleLoginModal={jest.fn()} logoutUser={jest.fn()} />);
+
+    const badMovieTitle = screen.getByText('Bad Movie');
+    const badMovieLogo = screen.getByAltText('Bad Movie Logo');
+    const loginButton = screen.getByText('Login');
+
+    expect(badMovieTitle).toBeInTheDocument();
+    expect(badMovieLogo).toBeInTheDocument();
+    expect(loginButton).toBeInTheDocument();
+  }));
