cabb36cd6fedcae7a3467325daa616f21239f164
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-08-23 13:31:42 -0600

    Add test for rendering correct content

--- src/Movie-Page.test.js
+++ src/Movie-Page.test.js
@@ -7,21 +7,21 @@ describe('Header Component', () => {
   let movieSelected
   beforeEach(() => {
     movieSelected = {
-        "id": 1,
-        "title": "Akira",
-        "poster_path": "https://image.tmdb.org/t/p/original//5KlRFKKSbyCiyYpZSS3A6G5bW0K.jpg",
-        "backdrop_path": "https://image.tmdb.org/t/p/original//qZ4NYuwME0j6QgJmIE6AZMgmCaj.jpg",
-        "release_date": "1988-07-16",
-        "overview": "A secret military project.",
-        "genres": [
+        id: 1,
+        title: "Akira",
+        poster_path: "https://image.tmdb.org/t/p/original//5KlRFKKSbyCiyYpZSS3A6G5bW0K.jpg",
+        backdrop_path: "https://image.tmdb.org/t/p/original//qZ4NYuwME0j6QgJmIE6AZMgmCaj.jpg",
+        release_date: "1988-07-16",
+        overview: "A secret military project.",
+        genres: [
             "Science Fiction",
             "Animation"
         ],
-        "budget": 10000000,
-        "revenue": 0,
-        "runtime": 100,
-        "tagline": "Neo-Tokyo is about to E.X.P.L.O.D.E.",
-        "average_rating": 6
+        budget: 10000000,
+        revenue: 0,
+        runtime: 100,
+        tagline: "E.X.P.L.O.D.E.",
+        average_rating: 6
     }
   });
 
@@ -29,6 +29,14 @@ describe('Header Component', () => {
   it('Should have the correct content when rendered without a current User', () => {
     render(<MoviePage movie={movieSelected} toggleMoviePage={jest.fn()} />);
 
-    expect().toBeInTheDocument();
+    const movieTitle = screen.getByRole('heading', { name: 'Akira' });
+    const movieTagline = screen.getByRole('heading', { name: 'E.X.P.L.O.D.E.' })
+    const movieOverview = screen.getByText('A secret military project.')
+    const movieRating = screen.getByRole('heading', { name: 'Ratings' })
+
+    expect(movieTitle).toBeInTheDocument();
+    expect(movieTagline).toBeInTheDocument();
+    expect(movieOverview).toBeInTheDocument();
+    expect(movieRating).toBeInTheDocument();
   });
 });
