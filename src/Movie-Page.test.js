eea77c07b99aa6e83c71e2b97eb35f7b34dff295
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-08-23 13:13:30 -0600

    Add beforeEach with sample data and begin first test

--- src/Movie-Page.test.js
+++ src/Movie-Page.test.js
@@ -1,4 +1,34 @@
 import React from 'react';
-import Header from './Header';
+import MoviePage from './Movie-Page';
 import { screen, fireEvent, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
+
+describe('Header Component', () => {
+  let movieSelected
+  beforeEach(() => {
+    movieSelected = {
+        "id": 1,
+        "title": "Akira",
+        "poster_path": "https://image.tmdb.org/t/p/original//5KlRFKKSbyCiyYpZSS3A6G5bW0K.jpg",
+        "backdrop_path": "https://image.tmdb.org/t/p/original//qZ4NYuwME0j6QgJmIE6AZMgmCaj.jpg",
+        "release_date": "1988-07-16",
+        "overview": "A secret military project.",
+        "genres": [
+            "Science Fiction",
+            "Animation"
+        ],
+        "budget": 10000000,
+        "revenue": 0,
+        "runtime": 100,
+        "tagline": "Neo-Tokyo is about to E.X.P.L.O.D.E.",
+        "average_rating": 6
+    }
+  });
+
+
+  it('Should have the correct content when rendered without a current User', () => {
+    render(<MoviePage movie={movieSelected} toggleMoviePage={jest.fn()} />);
+
+    expect().toBeInTheDocument();
+  });
+});
