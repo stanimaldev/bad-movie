ba37c95227bcb0074ab0bf30c68a5aa93cb3b1b4
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-08-23 09:25:47 -0600

    Remove old commented out code

--- src/Movie-Page.js
+++ src/Movie-Page.js
@@ -3,21 +3,6 @@ import './Movie-Page.css';
 import Fetch from './Fetch';
 
 const MoviePage = ( { movie } ) => {
-  // console.log(movie)
-  // const movie = {
-  //   title: movie.title,
-  //   poster_path: 'https://image.tmdb.org/t/p/original//5KlRFKKSbyCiyYpZSS3A6G5bW0K.jpg',
-  //   backdrop_path: 'https://image.tmdb.org/t/p/original//qZ4NYuwME0j6QgJmIE6AZMgmCaj.jpg',
-  //   release_date: '1988-07-16',
-  //   overview: 'A secret military project endangers Neo-Tokyo when it turns a biker gang member into a rampaging psychic psychopath that only two teenagers and a group of psychics can stop.',
-  //   genres: ['Science Fiction', 'Animation'],
-  //   budget: 10000000,
-  //   revenue: 0,
-  //   runtime: 124,
-  //   tagline: 'Neo-Tokyo is about to E.X.P.L.O.D.E.',
-  //   average_rating: 9,
-  // };
-
   return (
     <section className='movie-page' style={{ backgroundImage: `url(${movie.backdrop_path})` }}>
       <button className='movie-page-back-button'>Back</button>
