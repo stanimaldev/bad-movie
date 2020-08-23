f4bd9091cfdd904134436a142e338fd52bfd44d3
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-08-23 09:26:02 -0600

    Remove old commented out code

--- src/Movie-card.js
+++ src/Movie-card.js
@@ -2,14 +2,6 @@ import React, { Component } from 'react';
 import './Movie-Card.css';
 import Fetch from './Fetch';
 
-// const MovieCard = ({ movie, displayMoviePage }) => {
-//   return (
-//     <article className='movie-card' style={{ backgroundImage: `url(${movie.poster_path})` }} alt={`background image of ${movie.title} poster`} onClick={displayMoviePage}>
-//       <p className='movie-card-rating'>{movie.average_rating}/10</p>
-//     </article>
-//   );
-// };
-
 class MovieCard extends Component {
   constructor(props) {
     super(props)
