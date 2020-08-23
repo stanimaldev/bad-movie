a8b30e6173f0def337d000c72ee6259c9c3bc0f7
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-08-22 19:56:45 -0600

    Update movie page to be a class component and add getMovieToDisplay method

--- src/Movie-card.js
+++ src/Movie-card.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Movie-Card.css';
 
-const MovieCard = ({ movie, displayMoviePage }) => {
-  return (
-    <article className='movie-card' style={{ backgroundImage: `url(${movie.poster_path})` }} alt={`background image of ${movie.title} poster`} onClick={displayMoviePage}>
+// const MovieCard = ({ movie, displayMoviePage }) => {
+//   return (
+//     <article className='movie-card' style={{ backgroundImage: `url(${movie.poster_path})` }} alt={`background image of ${movie.title} poster`} onClick={displayMoviePage}>
+//       <p className='movie-card-rating'>{movie.average_rating}/10</p>
+//     </article>
+//   );
+// };
+
+class MovieCard extends Component {
+  constructor(props) {
+    super(props) {
+
+    }
+  }
+
+  getMovieToDisplay() {
+    const movieId = this.fetch.getSingleMovie(movie.id);
+    this.props.displayMoviePage;
+  }
+
+  render() {
+    return (
+      <article className='movie-card' style={{ backgroundImage: `url(${movie.poster_path})` }} alt={`background image of ${movie.title} poster`} onClick={this.getMovieToDisplay}>
       <p className='movie-card-rating'>{movie.average_rating}/10</p>
-    </article>
-  );
+      </article>
+    );
+  }
 };
 
 export default MovieCard;
