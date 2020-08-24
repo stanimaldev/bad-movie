de0b200cde52fb61addc1f7fa932a182b195f3cf
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-08-23 19:53:22 -0600

    Add layout for test

--- src/Login.test.js
+++ src/Login.test.js
@@ -2,3 +2,12 @@ import React from 'react';
 import Login from './Login';
 import { screen, fireEvent, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
+
+describe('Login Component', () => {
+  beforeEach(() => {
+
+  });
+  it('', () => {
+
+  });
+})
