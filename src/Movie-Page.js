5b6e4838843284761bf4dcf18ecd93a74e0690ae
Tyler Haglund <relyt4me@gmail.com>
2020-08-23 19:06:08 -0600

    Conditionally render if the user has already rated

--- src/Movie-Page.js
+++ src/Movie-Page.js
@@ -11,8 +11,15 @@ class MoviePage extends Component {
     this.fetch = new Fetch();
   }
 
+  getUserMovieRating = () => {
+    return this.props.currentUser.ratings.find((rating) => {
+      return this.props.movie.id === rating.movie_id;
+    });
+  };
+
   render() {
     const { movie, toggleMoviePage, currentUser } = this.props;
+    const currentUsersRating = this.getUserMovieRating();
     return (
       <section className='movie-page' style={{ backgroundImage: `url(${movie.backdrop_path})` }}>
         <button className='movie-page-back-button' onClick={toggleMoviePage}>
@@ -25,14 +32,16 @@ class MoviePage extends Component {
           <p className='movie-description-stats'>
             Budget: {movie.budget} dollars<br></br>Runtime: {movie.runtime} minutes
           </p>
-          <p className='movie-description-ratings'> Average Rating: {movie.average_rating}/10</p>
+          <p className='movie-description-ratings'> Average Rating: {Math.round(movie.average_rating * 10) / 10}/10</p>
         </article>
         {currentUser && (
           <aside className='movie-page-rating-card'>
-            {/* <div>
-            <h2 className='rating-card-title'>My Ratings</h2>
-            <p className='rating-card-user-rating'>0/10</p>
-          </div> */}
+            {currentUsersRating && (
+              <div>
+                <h2 className='rating-card-title'>My Ratings</h2>
+                <p className='rating-card-user-rating'>{currentUsersRating.rating}/10</p>
+              </div>
+            )}
             {/* <div>
             <label for='my-rating' className='my-rating'>
               My Rating: {}
