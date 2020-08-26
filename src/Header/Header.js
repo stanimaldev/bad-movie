dfef28f00aec3ba182e1befee75c21858554823e
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-08-25 19:18:42 -0600

    Remove Bad Movie title because we now have the logo working

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './Header.css';
-import '../badlogo.png';
+import badlogo from '../badlogo.png';
 
 const Header = ({ currentUser, toggleLoginModal, logoutUser }) => {
   return (
     <header className='header'>
-      <img src='../badlogo.png' alt='Bad Movie Logo' height='50' width='50' />
-      <h1 className='title'>Bad Movie</h1>
+      <img src={badlogo} alt='Bad Movie Logo' height='50' width='50' />
       {currentUser && <h2>Hello {currentUser.name}</h2>}
       {currentUser && (
         <button className='logout-button' onClick={logoutUser}>
